5b91c877997bfd0322effab1fe38e829e7cfbeb7
ksiyeon27 <siyeonk27@gmail.com>
2022-07-12 15:52:57 +0900

    [Chore] / 추가

--- src/routes/VoiceRouter.ts
+++ src/routes/VoiceRouter.ts
@@ -4,6 +4,6 @@ import upload from '../config/multer';
 
 const router: Router = Router();
 
-router.post('', upload.single('voice'), VoiceController.uploadVoiceFileToS3); //form-data 중 'voice' 필드
+router.post('/', upload.single('voice'), VoiceController.uploadVoiceFileToS3); //form-data 중 'voice' 필드
 
 export default router;
