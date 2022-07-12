263e16438eaa790c3d92499365ba69499752635e
ksiyeon27 <siyeonk27@gmail.com>
2022-07-12 14:50:02 +0900

    [Fix] db.ts 바로 env 변수와 연결해 봄

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -3,7 +3,7 @@ import config from "../config";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.mongo_URI);
+    await mongoose.connect(process.env.MONGODB_URI as string);
 
     mongoose.set("autoCreate", true);
 
