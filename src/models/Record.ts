a140633cd5234ae709afc991ac755cda4f67f95a
ksiyeon27 <siyeonk27@gmail.com>
2022-07-15 13:57:56 +0900

    [Chore] data는 response에서 제외

--- src/models/Record.ts
+++ src/models/Record.ts
@@ -2,41 +2,46 @@ import mongoose from 'mongoose';
 import { RecordInfo } from '../interfaces/record/RecordInfo';
 import userMocking from './UserMocking';
 
-const RecordSchema = new mongoose.Schema({
-  writer: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    default: userMocking,
+const RecordSchema = new mongoose.Schema(
+  {
+    writer: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      default: userMocking,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    voice: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Voice',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    emotion: {
+      type: Number,
+      default: 0,
+    },
+    dream_color: {
+      type: Number,
+      default: 0,
+    },
+    genre: {
+      type: Array,
+    },
+    note: {
+      type: String,
+    },
   },
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
-  voice: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Voice',
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-  },
-  emotion: {
-    type: Number,
-    default: 0,
-  },
-  dream_color: {
-    type: Number,
-    default: 0,
-  },
-  genre: {
-    type: Array,
-  },
-  note: {
-    type: String,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 export default mongoose.model<RecordInfo & mongoose.Document>('Record', RecordSchema);
