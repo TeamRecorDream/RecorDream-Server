db545e29a77aa95731aa74443ded36d71dbf1098
ksiyeon27 <siyeonk27@gmail.com>
2022-07-15 22:20:13 +0900

    [Chore] express 지움 흥

--- src/controllers/VoiceController.ts
+++ src/controllers/VoiceController.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
