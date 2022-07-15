59500592655cc922ef345c6b2a71f63beb96b8cb
ksiyeon27 <siyeonk27@gmail.com>
2022-07-15 22:15:48 +0900

    [Chore] responseMessage 수정

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -16,7 +16,7 @@ import { RecordUpdateDto } from '../interfaces/record/RecordUpdateDto';
 const createRecord = async (req: Request, res: Response) => {
   const err = validationResult(req);
   if (!err.isEmpty()) {
-    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.RECORD_CREATE_TITLE_FAIL));
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.CREATE_RECORD_TITLE_FAIL));
   }
 
   req.body.date = dayjs(req.body.date).format('YYYY-MM-DD');
@@ -25,7 +25,7 @@ const createRecord = async (req: Request, res: Response) => {
   try {
     const data = await RecordService.createRecord(recordCreateDto);
 
-    res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.RECORD_CREATE_SUCCESS, data));
+    res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_RECORD_SUCCESS, data));
   } catch (err) {
     console.log(err);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
@@ -60,7 +60,7 @@ const getRecord = async (req: Request, res: Response) => {
 const updateRecord = async (req: Request, res: Response) => {
   const error = validationResult(req); //title empty 확인?
   if (!error.isEmpty()) {
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.RECORD_UPDATE_FAIL));
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.UPDATE_RECORD_FAIL));
   }
 
   const recordUpdateDto: RecordUpdateDto = req.body;
