5c2fe2f12b7c14a36658599caca0501da6122184
Choo <choo000407@naver.com>
2022-07-17 00:52:29 +0900

    [Merge] Develop merge

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-import { RecordCreateDto } from '../interfaces/record/RecordCreateDto';
-import message from '../modules/responseMessage';
-import statusCode from '../modules/statusCode';
-import util from '../modules/util';
-import RecordService from '../services/RecordService';
-import dayjs from 'dayjs';
-import { RecordUpdateDto } from '../interfaces/record/RecordUpdateDto';
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { RecordCreateDto } from "../interfaces/record/RecordCreateDto";
+import message from "../modules/responseMessage";
+import statusCode from "../modules/statusCode";
+import util from "../modules/util";
+import RecordService from "../services/RecordService";
+import dayjs from "dayjs";
+import { RecordUpdateDto } from "../interfaces/record/RecordUpdateDto";
 
 /**
  * @route POST /record
@@ -19,7 +19,7 @@ const createRecord = async (req: Request, res: Response) => {
     return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.CREATE_RECORD_TITLE_FAIL));
   }
 
-  req.body.date = dayjs(req.body.date).format('YYYY-MM-DD');
+  req.body.date = dayjs(req.body.date).format("YYYY-MM-DD");
   const recordCreateDto: RecordCreateDto = req.body;
 
   try {
@@ -57,8 +57,8 @@ const getRecord = async (req: Request, res: Response) => {
  *  @desc Get RecordList
  *  @access Public
  */
-const getRecordList = async(req: Request, res: Response) => {
-  const userId = req.header('userId');
+const getRecordList = async (req: Request, res: Response) => {
+  const userId = req.header("userId");
   try {
     if (!userId) {
       res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
@@ -76,7 +76,7 @@ const getRecordList = async(req: Request, res: Response) => {
   }
 };
 
-/** 
+/**
  *  @route PATCH /record/:recordId
  *  @desc update Record
  *  @access Public
@@ -125,9 +125,9 @@ const deleteRecord = async (req: Request, res: Response) => {
  *  @desc Get RecordStorage
  *  @access Public
  */
- const getRecordStorage = async(req: Request, res: Response) => {
+const getRecordStorage = async (req: Request, res: Response) => {
   const { filter } = req.query;
-  const userId = req.header('userId');
+  const userId = req.header("userId");
   try {
     if (!userId) {
       res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
--- src/routes/RecordRouter.ts
+++ src/routes/RecordRouter.ts
@@ -1,46 +1,46 @@
-import { Router } from 'express';
-import RecordController from '../controllers/RecordController';
-import { body } from 'express-validator/check';
-import { check } from 'express-validator/check';
+import { Router } from "express";
+import RecordController from "../controllers/RecordController";
+import { body } from "express-validator/check";
+import { check } from "express-validator/check";
 
 const router: Router = Router();
 
 //title null 안됨, 공백만 안됨, 공백 제외 글자수 25까지 가능. voice와 writer는 수정불가
 router.post(
-  '/',
+  "/",
   [
-    body('title').exists(),
-    body('title')
+    body("title").exists(),
+    body("title")
       .custom((title) => {
-        if (title.toString().replace(/(\s*)/g, '').length > 25 || title.toString().replace(/(\s*)/g, '').length < 1) {
+        if (title.toString().replace(/(\s*)/g, "").length > 25 || title.toString().replace(/(\s*)/g, "").length < 1) {
           return false;
         }
         return true;
       })
-      .withMessage('제목 오류'),
+      .withMessage("제목 오류"),
   ],
   RecordController.createRecord
 );
-router.get('/:recordId', RecordController.getRecord);
-router.get('/', RecordController.getRecordList);
+router.get("/:recordId", RecordController.getRecord);
+router.get("/", RecordController.getRecordList);
 router.patch(
-  '/:recordId',
+  "/:recordId",
   [
-    check('title')
-      .if(body('title').exists())
+    check("title")
+      .if(body("title").exists())
       .custom((title) => {
-        if (title.toString().replace(/(\s*)/g, '').length > 25 || title.toString().replace(/(\s*)/g, '').length < 1) {
+        if (title.toString().replace(/(\s*)/g, "").length > 25 || title.toString().replace(/(\s*)/g, "").length < 1) {
           return false;
         }
         return true;
       })
-      .withMessage('제목 오류'),
-    body('voice').not().exists(),
-    body('writer').not().exists(),
+      .withMessage("제목 오류"),
+    body("voice").not().exists(),
+    body("writer").not().exists(),
   ],
   RecordController.updateRecord
 );
-router.delete('/:recordId', RecordController.deleteRecord);
-router.get('/storage/list', RecordController.getRecordStorage);
+router.delete("/:recordId", RecordController.deleteRecord);
+router.get("/storage/list", RecordController.getRecordStorage);
 
 export default router;
