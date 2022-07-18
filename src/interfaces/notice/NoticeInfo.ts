bb523c4429e4f537eb994b1fefdb5b0e48cea2ff
Seokyeong <purewater7174@gmail.com>
2022-07-19 01:47:40 +0900

    [Del] 참조 삭제

--- src/interfaces/notice/NoticeInfo.ts
+++ src/interfaces/notice/NoticeInfo.ts
@@ -1,7 +1,6 @@
 import { UserInfo } from "../user/UserInfo";
 
 export interface NoticeInfo {
-  user_id: UserInfo;
   time: string;
   createdAt: string;
   is_changed: boolean;
--- src/models/Notice.ts
+++ src/models/Notice.ts
@@ -4,11 +4,6 @@ import { NoticeInfo } from "../interfaces/notice/NoticeInfo";
 import userMocking from "./UserMocking";
 
 const NoticeSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    default: userMocking,
-  },
   time: {
     type: String,
     default: dayjs().format("A hh:mm"),
