4811ab49e2bad9ae3c0a1a3182399df5fd8eabb7
ksiyeon27 <siyeonk27@gmail.com>
2022-07-20 02:22:41 +0900

    [Fix] allow null in recordResponseDto

--- src/interfaces/record/RecordResponseDto.ts
+++ src/interfaces/record/RecordResponseDto.ts
@@ -1,13 +1,13 @@
 import { VoiceResponseDto } from "../voice/VoiceResponseDto";
 
 export interface RecordResponseDto {
-  writer: string; // nickname
-  date: string; // 2022/06/26 (일)
+  writer: string | null; // nickname
+  date: string | null; // 2022/06/26 (일)
   voice: VoiceResponseDto | null;
   title: string;
-  content: string;
-  emotion: number;
-  dream_color: number;
-  genre: number[];
-  note: string;
+  content: string | null;
+  emotion: number | null;
+  dream_color: number | null;
+  genre: number[] | null;
+  note: string | null;
 }
