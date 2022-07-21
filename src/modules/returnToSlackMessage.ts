71f637c8c7a0d8180f83bdff5cb5f364dff5af23
ksiyeon27 <siyeonk27@gmail.com>
2022-07-22 05:05:33 +0900

    [Chore] error.message로

--- src/modules/returnToSlackMessage.ts
+++ src/modules/returnToSlackMessage.ts
@@ -1,5 +1,5 @@
 import { sendMessageToSlack } from "./slackAPI";
 
 export const slackMessage = (method: string, originalUrl: string, error: any, uid?: number): string => {
-  return `[ERROR] [${method}] ${originalUrl} ${uid ? `uid: ${uid}` : "req.user 없음"} ${JSON.stringify(error)}`;
+  return `[ERROR] [${method}] ${originalUrl} ${uid ? `uid: ${uid}` : "req.user 없음"} ${JSON.stringify(error.message)}`;
 };
