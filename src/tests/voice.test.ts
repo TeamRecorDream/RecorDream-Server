5203637ccdb0aeb7be7d3e94631d563c2259a663
ksiyeon27 <siyeonk27@gmail.com>
2022-07-22 03:35:12 +0900

    [Chore]  깃훅 실험2

--- src/tests/voice.test.ts
+++ src/tests/voice.test.ts
@@ -7,7 +7,7 @@ describe("[POST] /voice", () => {
   //201 CREATED
   it("음성 녹음 업로드 테스트", async () => {
     const res = await req.post("/voice").set("Content-Type", "application/json").set("userId", "1");
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(200);
     expect(res.body.message).toEqual("음성 녹음 업로드 성공");
     expect(res.body.data).toEqual({
       _id: "62d1745119a54968acdd7270",
