300712723cc98f2ede5f687e747f7efa7fb681cd
Seokyeong <purewater7174@gmail.com>
2022-07-23 06:53:46 +0900

    [Fix] 꿈기록 작성 날짜

--- src/models/Record.ts
+++ src/models/Record.ts
@@ -11,7 +11,7 @@ const RecordSchema = new mongoose.Schema(
     },
     date: {
       type: Date,
-      default: Date.now(),
+      //default: Date.now(),
     },
     voice: {
       type: mongoose.Types.ObjectId,
