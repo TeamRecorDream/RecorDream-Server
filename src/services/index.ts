24c86ef0cb8b2970b71b013bc2ffdb5e1d7a45f4
Seokyeong <purewater71@naver.com>
2022-10-02 04:54:53 +0900

    [Add] index 연결

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@ import UserService from "./UserService";
 import RecordService from "./RecordService";
 import VoiceService from "./VoiceService";
 import NoticeService from "./NoticeService";
+import AuthService from "./AuthService";
 
-export { UserService, RecordService, VoiceService, NoticeService };
+export { UserService, RecordService, VoiceService, NoticeService, AuthService };
