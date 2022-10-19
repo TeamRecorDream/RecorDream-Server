c2e9fbb62d708f367665fa4e9d2fec157341ff83
Seokyeong <purewater71@naver.com>
2022-10-19 13:17:24 +0900

    [Feat] router 연결

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import { UserController } from "../controllers";
+import auth from "../middleware/auth";
 
 const router: Router = Router();
 
@@ -8,5 +9,6 @@ router.get("/:token", UserController.getUser);
 router.put("/nickname", [body("nickname").notEmpty().isLength({ min: 1, max: 8 })], UserController.updateNickname);
 router.put("/fcm-token", UserController.updateFcmToken);
 router.put("/:toggle", UserController.changeToggle);
+router.delete("/", auth, UserController.deleteUser);
 
 export default router;
