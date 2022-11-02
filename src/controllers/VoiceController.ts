1ddfd2183a12ada4218481fe68d2b4d50265ddc3
ksiyeon27 <siyeonk27@gmail.com>
2022-11-03 00:42:46 +0900

    [Refactor] VoiceController

--- src/controllers/VoiceController.ts
+++ src/controllers/VoiceController.ts
@@ -12,8 +12,13 @@ import { slackMessage } from "../modules/returnToSlackMessage";
  *  @access Public
  */
 const uploadVoiceFileToS3 = async (req: Request, res: Response) => {
+  if (!req.file) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+
+  const voice: Express.MulterS3.File = req.file as Express.MulterS3.File; //req.file은 기본 Express.Multer.File 타입으로 추론되어서 s3버전으로 타입 단언 필요.
+  const { originalname, location } = voice;
+
   try {
-    const data = await VoiceService.createVoice();
+    const data = await VoiceService.createVoice(location, originalname);
     res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.UPLOAD_VOICE_SUCCESS, data));
   } catch (err) {
     console.log(err);
