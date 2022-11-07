5906688f5c3091e6758dbedee41f574241f98d19
Seokyeong <purewater71@naver.com>
2022-11-07 17:26:41 +0900

    [Fix] 앱잼용 디바이스 토큰 삭제로 인한 check fail 해결

--- src/tests/getUser.test.ts
+++ src/tests/getUser.test.ts
@@ -8,14 +8,10 @@ GET /user
 
 describe("[GET] /user", () => {
   const req = request(app);
-  const fcm_token_ios = config.fcm_token_ios;
 
   //200 OK - 유일 유저로
   it("유저 조회 테스트", async () => {
-    const res = await req
-      .get("/user/" + fcm_token_ios)
-      .set("Content-Type", "application/json")
-      .set("userId", "1");
+    const res = await req.get("/user").set("Content-Type", "application/json").set("userId", "1");
     expect(res.status).toBe(200);
     expect(res.body.data).toEqual({
       nickname: "레코드림",
@@ -27,7 +23,7 @@ describe("[GET] /user", () => {
 
   //400 Bad Request - userId 빼고 리퀘스트
   it("유저 조회 테스트(없는 id)", async () => {
-    const res = await req.get("/user/" + fcm_token_ios).set("Content-Type", "application/json");
+    const res = await req.get("/user").set("Content-Type", "application/json");
     expect(res.status).toBe(400);
     expect(res.body.message).toEqual("필요한 값이 없습니다.");
   });
