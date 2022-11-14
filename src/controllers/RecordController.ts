8d55fc1eb515b2550cf1e402bcaa28821a11d89e
ksiyeon27 <siyeonk27@gmail.com>
2022-11-15 02:26:50 +0900

    [Refactor] post record without dream_color

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -21,6 +21,8 @@ const createRecord = async (req: Request, res: Response) => {
     return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.CREATE_RECORD_TITLE_FAIL));
   }
 
+  const userId = req.body.user.id;
+  req.body.writer = userId;
   req.body.date = dayjs(req.body.date).format("YYYY-MM-DD");
   const recordCreateDto: RecordCreateDto = req.body;
 
@@ -30,7 +32,7 @@ const createRecord = async (req: Request, res: Response) => {
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.CREATE_RECORD_NUMBER_FAIL));
     }
 
-    return res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_RECORD_SUCCESS, data));
+    res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_RECORD_SUCCESS, data));
   } catch (err) {
     console.log(err);
     const errorMessage: string = slackMessage(req.method.toUpperCase(), req.originalUrl, err, req.body.user?.id);
