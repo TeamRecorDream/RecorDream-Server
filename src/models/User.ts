e4e86bd619e71c0da83885839bcd1f779d237a70
Seokyeong <purewater71@naver.com>
2023-01-08 08:22:48 +0900

    [Add] User 모델 버전키 제거

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,45 +2,50 @@ import dayjs from "dayjs";
 import mongoose from "mongoose";
 import { UserInfo } from "../interfaces/user/UserInfo";
 
-const UserSchema = new mongoose.Schema({
-  appleId: {
-    type: String,
+const UserSchema = new mongoose.Schema(
+  {
+    appleId: {
+      type: String,
+    },
+    nickname: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+    },
+    ageRange: {
+      type: String,
+    },
+    isAlreadyUser: {
+      type: Boolean,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    fcmTokens: {
+      type: [String],
+    },
+    time: {
+      type: String,
+      default: dayjs().format("A hh:mm"),
+    },
+    isActive: {
+      type: Boolean,
+    },
   },
-  nickname: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  gender: {
-    type: String,
-  },
-  ageRange: {
-    type: String,
-  },
-  isAlreadyUser: {
-    type: Boolean,
-  },
-  accessToken: {
-    type: String,
-    required: true,
-  },
-  refreshToken: {
-    type: String,
-    required: true,
-  },
-  fcmTokens: {
-    type: [String],
-  },
-  time: {
-    type: String,
-    default: dayjs().format("A hh:mm"),
-  },
-  isActive: {
-    type: Boolean,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 export default mongoose.model<UserInfo & mongoose.Document>("User", UserSchema);
