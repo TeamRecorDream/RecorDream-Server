590f4398a466baeba6eb10015d8871c50471d7bb
Seokyeong <purewater71@naver.com>
2023-01-09 18:17:01 +0900

    [Del] 주석 제거

--- src/interfaces/user/UserResponseDto.ts
+++ src/interfaces/user/UserResponseDto.ts
@@ -2,5 +2,5 @@ export interface UserResponseDto {
   nickname: string;
   email: string;
   isActive: boolean;
-  time: string | null; // isActive가 false인 경우, time은 항상 null
+  time: string | null;
 }
